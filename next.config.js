390975d3ec32315230aa8dbf985272cf9041c829
cathe-zhang <zhangle_media@hotmail.com>
2019-09-20 16:54:24 +0800

    chore: update assetPrefix

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const withLess = require('@zeit/next-less')
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = withLess({
-  // assetPrefix: isProd ? '/my_next' : '',
-  // publicRuntimeConfig: {
-  //   linkPrefix: isProd ? '/my_next' : ''
-  // }
+  assetPrefix: isProd ? '/my_next' : '',
+  publicRuntimeConfig: {
+    linkPrefix: isProd ? '/my_next' : ''
+  }
 })
